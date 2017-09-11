6e55b379aa1a38f80407aee5566a033313ad549b
eladFrizi <okc.elad.35@gmail.com>
2017-09-11 13:13:13 +0300

    updates in gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,38 @@ var useref = require('gulp-useref');
 var cssnano = require('gulp-cssnano');
 var imagemin = require('gulp-imagemin');
 var replace = require('gulp-replace');
-
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 
-gulp.task('css', function(){
-    return gulp.src('./*.html')
-      .pipe(useref())
-      .pipe(gulpIf('*.js', uglify()))
-      // Minifies only if it's a CSS file
-      .pipe(gulpIf('*.css', cssnano()))
-      .pipe(gulp.dest('dist'))
-  });
-  
-  gulp.task('images', function(){   
-    return gulp.src('images/**/*.+(png|jpg|gif|svg)')
+gulp.task('all', function () {
+  return gulp.src('*.+(html|php)')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('images', function () {
+  return gulp.src('images/**/*.+(png|jpg|gif|svg)')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
-  });
-  
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('css/fonts/*.+(*)')
+    .pipe(gulp.dest('dist/fonts/'));
+});
+
+gulp.task('swipercss', function () {
+  return gulp.src('css/swiper.css')
+    .pipe(gulp.dest('dist/css'))
+})
+gulp.task('data', function () {
+  return gulp.src('projs.json')
+    .pipe(gulp.dest('dist'))
+})
+
   // gulp.task('js', function(){
   //   return gulp.src('./*.html')
   //     .pipe(useref())
@@ -29,4 +42,3 @@ gulp.task('css', function(){
   //     .pipe(gulpIf('*.js', uglify()))
   //     .pipe(gulp.dest('dist'))
   // });
-  
